50db1ca4b52aad53a159a1ce2bb2158a850f1e89
LindorCarl <lindorcarl8@gmail.com>
2023-04-18 16:01:16 +0200

    Update commit

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import '../../styles/index.scss';
+import Banner from '../../components/HomeBanner/Banner';
+import Apartment from "../../components/Apartment/Apartment";
+
+function Home() {
+  return (
+    <div>
+        <Banner/>
+        <Apartment/>
+    </div>
+  );
+}
+
+export default Home;
